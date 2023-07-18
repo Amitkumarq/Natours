d6aae1c48fbd8e18774603faeb09ec039e723d8a
Amitkumarq <ak1510242@gmail.com>
2023-07-18 18:40:38 +0530

    second commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,11 +3,44 @@ const Tour = require('../Models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // Build Query
-    const queryObj = { ...req.body };
+    const queryObj = { ...req.query };
+    //console.log(queryObj);
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // adv query
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v'); //  '-' is used here to exclude the field
+    }
+
+    // Pagination
+    const page = req.query.page * 1 || 1; // * 1 will simply convert string into an integer
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does next exists');
+    }
 
     // Execute Query
     const tours = await query;
